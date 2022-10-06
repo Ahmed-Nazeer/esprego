e09528759b097d2dc59fb3dad420f060ff64bd98
ahmednazeer <f200282@cfd.nu.edu.pk>
2022-10-06 16:34:06 +0500

    third-commit

--- controller.js
+++ controller.js
@@ -1,13 +1,40 @@
-const express=require('express')
-const app=express()
-const path=require("path")
-const port=3000
-app.use(express.static(path.join(__dirname,'view')))
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname,'view','home.html'))
-    
-  })
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+var express = require('express');
+var app = express();
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+// use res.render to load up an ejs view file
+// index page
+app.get('/', function(req, res) {
+  // res.render('page/home');
+  res.sendFile('page/home');
+});
+
+app.listen(8080);
+console.log('Server is listening on port 8080');
   
+
+// const express=require('express')
+// const app=express()
+// const path=require("path")
+// const port=3000
+// app.use(express.static(path.join(__dirname,'views')))
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname,'views','home.html'))
+    
+//   })
+//   app.get('/home', (req, res) => {
+//     res.sendFile(path.join(__dirname,'views','home.html'))
+    
+//   })
+//   app.get('/about', (req, res) => {
+//     res.sendFile(path.join(__dirname,'views','about.html'))
+    
+//   })
+//   app.get('/contact', (req, res) => {
+//     res.sendFile(path.join(__dirname,'views','contact.html'))
+    
+//   })
+//   app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`)
+//   })
+
