40faeae72174ba67fc2f26ce6567d77471018096
ahmednazeer <f200282@cfd.nu.edu.pk>
2022-10-09 22:32:57 +0500

    fourth-commit

--- controller.js
+++ controller.js
@@ -1,40 +1,60 @@
-var express = require('express');
-var app = express();
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-// use res.render to load up an ejs view file
-// index page
-app.get('/', function(req, res) {
-  // res.render('page/home');
-  res.sendFile('page/home');
-});
+// var express = require('express');
+// var app = express();
+// // set the view engine to ejs
+// app.set('view engine', 'ejs');
+// // use res.render to load up an ejs view file
+// // index page
+// app.get('/', function(req, res) {
+// //   // res.render('page/home');
+// //   res.sendFile('page/home');
+// });
 
-app.listen(8080);
-console.log('Server is listening on port 8080');
+// app.listen(8080);
+// console.log('Server is listening on port 8080');
   
 
-// const express=require('express')
-// const app=express()
-// const path=require("path")
-// const port=3000
-// app.use(express.static(path.join(__dirname,'views')))
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname,'views','home.html'))
+const express=require('express')
+const app=express()
+const path=require("path")
+const mysql = require('mysql'); //both dependencies are present
+const port=3000
+app.set('view engine', 'ejs');
+var connection=mysql.createConnection({
+host:'localhost',
+user:'root',
+password:'',
+database:'webproject'
+});
+connection.connect(function (err){
+  if (err) throw err;
+  console.log('connected');
+})
+app.use(express.static(path.join(__dirname,'views')))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'views','home.html'))
     
-//   })
-//   app.get('/home', (req, res) => {
-//     res.sendFile(path.join(__dirname,'views','home.html'))
+  })
+  app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname,'views','home.html'))
     
-//   })
-//   app.get('/about', (req, res) => {
-//     res.sendFile(path.join(__dirname,'views','about.html'))
+  })
+  app.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname,'views','about.html'))
     
-//   })
-//   app.get('/contact', (req, res) => {
-//     res.sendFile(path.join(__dirname,'views','contact.html'))
+  })
+  app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname,'views','contact.html'))
     
-//   })
-//   app.listen(port, () => {
-//     console.log(`Example app listening on port ${port}`)
-//   })
+  })
+  app.get('/routes', (req, res) => {
+    var query="select * from customer";
+    connection.query(query,(err,row,fields)=>{
+      if (err) throw err;
+      res.render('page/index',{title:'Customer Record', action:'list',data:row});
+    })
+    
+  })
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
 
